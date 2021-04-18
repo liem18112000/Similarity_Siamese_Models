4dee1a2c36e6ef3a8a12ae0be97a00ddfd68b4a4
Liêm <1859027@itec.hcmus.edu.vn>
2021-04-18 16:37:55 +0700

    update

--- main.py
+++ main.py
@@ -1,25 +1,89 @@
-from models.models import SimilarityModel
 from models.factory import FeatureExtractModelFactory,  SiameseModelFactory
 from loader.loader import Loader
 import numpy as np
-import pytest
+from models.utility import preprocess_data_into_groups
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 IMAGE_SIZE = 32
 
+def prepare_data():
 
-def main():
     loader = Loader()
+
+    train_images, train_labels, test_images, test_labels = loader.load("fashion_mnist")
+
+    train_groups, test_groups = preprocess_data_into_groups(train=(train_images, train_labels), test=(test_images, test_labels), image_size=IMAGE_SIZE)
+
+    return train_groups, test_groups
+
+
+
+def create_model():
+
     extractor_factory = FeatureExtractModelFactory((IMAGE_SIZE, IMAGE_SIZE, 1))
-    simi_factory = SiameseModelFactory(
+
+    factory = SiameseModelFactory(
         extractor_factory
     )
 
-    model = SimilarityModel(simi_factory)
-    train_groups, test_groups = model.preprocess(loader.load("fashion_mnist"))
-    model.compile()
-    model.train(train_groups, test_groups)
-    model.evaluate(test_groups)
-    model.plotHistory()
+    _model = factory.createModel()
+
+    _generator = factory.createDataGenerator()
+
+    _loss = factory.createLossFunction()
+
+    return _model, _generator, _loss
+
+
+
+def plot(history):
+
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def main():
+
+    train_groups, test_groups = prepare_data()
+
+    _model, _generator, _loss= create_model()
+
+    # setup the optimization process
+    _model.compile(
+        optimizer='adam',
+        loss=_loss,
+        metrics=['mae', 'acc']
+    )
+
+    # we want a constant validation group to have a frame of reference for model performance
+    valid_a, valid_b, valid_sim = _generator(test_groups, 1024)
+
+    history = _model.fit(
+        _generator(train_groups),
+        steps_per_epoch=100,
+        validation_data=([valid_a, valid_b], valid_sim),
+        epochs=20,
+        callbacks=[tf.keras.callbacks.EarlyStopping(
+            monitor='loss', patience=4, restore_best_weights=True)]
+    )
+
+    plot(history)
 
 
 if __name__ == "__main__":
